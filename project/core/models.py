8e44b62e74439d254a3ffd43dadc7ea25c2cc624
Daniel Owen <danielowen1@hotmail.co.uk>
2021-06-10 11:36:09 +0100

    Use new binance_client to import and update symbols and spot orders

--- project/core/models.py
+++ project/core/models.py
@@ -1,8 +1,9 @@
 from binance import Client
+from django.core.exceptions import ValidationError
 from django.db import models
-
-# Create your models here.
 from django.db.models import Q
+
+from core import binance_client
 
 
 class RelevantSpotOrder(models.Manager):
@@ -22,6 +23,27 @@ class Symbol(models.Model):
 
     objects = models.Manager()
     relevant = RelevantSpotOrder()
+
+    def clean(self):
+        if binance_client.fetch_symbol_price_if_exists(self.symbol) is None:
+            raise ValidationError('%s is an invalid symbol' % self.symbol)
+
+    @staticmethod
+    def import_from_binance(*extra_base_assets):
+        symbols_to_fetch = list(set(binance_client.INITIAL_BASE_ASSETS) | set(map(str.upper, extra_base_assets)))
+        all_coins = binance_client.client().get_all_tickers()
+        Symbol.objects.bulk_create(
+            (Symbol(**symbol_info) for symbol_info in all_coins if any(
+                symbol_info['symbol'].startswith(symbols_to_fetch) for symbols_to_fetch in symbols_to_fetch))
+        )
+
+    @staticmethod
+    def update_prices_from_binance():
+        symbols = []
+        for symbol in Symbol.relevant.all():
+            symbol.price = binance_client.client().get_avg_price(symbol=symbol.symbol)['price']
+            symbols.append(symbol)
+        Symbol.objects.bulk_update(symbols, ['price'])
 
     def __str__(self):
         return '%s' % (self.symbol)
@@ -55,6 +77,36 @@ class SpotOrder(models.Model):
     side = models.CharField(choices=SIDE, max_length=5)
     timestamp = models.BigIntegerField()
     created_datetime = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def gains_losses(self):
+        if self.side == Client.SIDE_BUY:
+            prof = self.symbol.price - self.price
+        else:
+            prof = self.price - self.symbol.price
+        return prof * self.executed_quantity
+
+    @staticmethod
+    def import_from_binance():
+        symbols = Symbol.relevant.all()
+        for symbol in symbols:
+            orders = binance_client.get_all_orders(symbol=symbol.symbol)
+            for order in orders:
+                if order['status'] == Client.ORDER_STATUS_FILLED:
+                    a, created = SpotOrder.objects.update_or_create(
+                        order_id=order['orderId'],
+                        client_order_id=order['clientOrderId'],
+                        symbol=symbol,
+                        defaults={
+                            'price': order['price'],
+                            'executed_quantity': order['executedQty'],
+                            'cummulative_quote_quantity': order['cummulativeQuoteQty'],
+                            'status': order['status'],
+                            'timestamp': order['time'],
+                            'side': order['side']
+                        }
+                    )
+                    print(a, created)
 
     class Meta:
         ordering = ['symbol']
