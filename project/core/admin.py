9bdba60aa33944bb5e56ba27e77c0bc7bf7d3bbe
Daniel Owen <danielowen1@hotmail.co.uk>
2021-06-10 11:36:37 +0100

    ModelAdmins for SpotOrder and Symbol

--- project/core/admin.py
+++ project/core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from django.utils.html import format_html
+
+from core import models
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+
+class SymbolModelAdmin(BaseModelAdmin):
+    # form = SymbolModelAdminForm
+
+    fields = ['id', 'symbol', 'price', 'updated_datetime']
+    list_display = ['symbol', 'price', 'updated_datetime']
+    readonly_fields = ['id', 'price', 'updated_datetime']
+
+
+class SpotOrderModelAdmin(BaseModelAdmin):
+    fields = ['id', 'order_id', 'client_order_id', 'symbol', 'price', 'executed_quantity', 'cummulative_quote_quantity', 'status', 'timestamp']
+    list_display = ['symbol', 'price', 'executed_quantity', 'gains_losses', 'side']
+    readonly_fields = ['id', 'order_id', 'client_order_id', 'symbol', 'price', 'executed_quantity', 'cummulative_quote_quantity', 'status', 'timestamp']
+    list_filter = ['side']
+
+    def gains_losses(self, obj):
+        color = 'AAB7B8'
+        if obj.gains_losses > 0:
+            color = '008000'
+        elif obj.gains_losses < 0:
+            color = 'FF3346'
+        return format_html('<span style="color: #{};">{}</span>', color, obj.gains_losses)
+
+
+admin.site.register(models.Symbol, SymbolModelAdmin)
+admin.site.register(models.SpotOrder, SpotOrderModelAdmin)
